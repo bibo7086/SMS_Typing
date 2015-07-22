760d613ae9150d65c71c4f08360f2679c099a0f2
Saeed Aliyu <saeedaliyu@Saeeds-MacBook-Pro.local>
2015-07-22 23:19:08 +0100

    Initial Commit

---
+++ SMS_Typing/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SMS_Typing
+//
+//  Created by Saeed Aliyu on 7/22/15.
+//  Copyright (c) 2015 Saeed Aliyu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
